5ed827bde1c8959b0d480f2e0e0e985eef831aa4
Samuel Dailey <36947121+Sdailey40@users.noreply.github.com>
2018-06-27 14:46:41 -0400

    Upload to repo

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "OkSriCct4ioBF02vDNojJetDn"
+consumer_secret = "dsGBqYSlAAK5aHwl1ASUe2JNR4NebUOTTyAX5qAHOSN2vJU4F2"
+access_token = "208090606-uwuMC4zxzMuEMktxwodDIV20XTsGXGYvHAusxsym"
+access_token_secret = "aXOTnsGGFRwSTHKDPCUtGu62vHF4npXIolaeHoVCSvm3f" 
